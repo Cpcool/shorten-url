312c1ada5cdb6f406574ac3280f3d6f2a73c959c
chanchal pareek <chanchal.pareek@habilelabs.io>
2021-01-17 21:26:04 +0530

    modified home page css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 let controller = require('../controllers');
 
 module.exports = (app) => {
+    app.get('/', controller.home)
     app.post('/api/v1/short-url', controller.saveUrl)
     app.get('/:url', controller.getLongUrl)
-    app.get('/', controller.home)
 }
